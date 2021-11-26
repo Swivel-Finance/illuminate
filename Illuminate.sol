71cade73e1aff32c82dc7b6e7e3e52a0d1186dee
Julian Traversa <unitedterrorhots@gmail.com>
2021-11-26 02:30:26 -0500

    gas optimizations and immutible addresses
    - mass transfers rather than individualized approvals/transfers across few functions
    - immutable yield/swivel addresses
    - added comments

--- Illuminate.sol
+++ Illuminate.sol
@@ -37,6 +37,7 @@ abstract contract SwivelMarketplace {
 
 abstract contract SwivelRouter {
     function initiate(Hash.Order[] calldata o, uint256[] calldata a, Sig.Components[] calldata c) public virtual returns(bool);
+    function redeemZcToken(address, uint256, uint256 ) virtual external returns (bool);
 }
 
 contract Illuminate {
@@ -48,30 +49,41 @@ contract Illuminate {
     }
     
     address public admin;
+    address public immutable yieldRouter;
+    address public immutable swivelRouter;
     
     mapping (address => mapping (uint256 => Market)) public markets;
     
     event marketCreated(address indexed underlying, uint256 indexed maturity, address indexed illuminate, address swivel, address yield);
     
-    constructor () {
+    constructor (address swivelAddress, address yieldAddress) {
         admin = msg.sender;
+        swivelRouter = swivelAddress;
+        yieldRouter = yieldAddress;
     }
     
-    
+
+    /// Can be called by the admin to create a new market of associated swivel, yield, and illuminate zero-coupon tokens (zcTokens, yTokens, lTokens)
+    /// underlying the address of the underlying token deposit
+    /// maturity the maturity of the market, it must be the identical across protocols or within a 1 day buffer
+    /// swivel the address of the swivel zcToken
+    /// yield the address of the yield token
+    /// decimals the number of decimals in the underlying token
     function createMarket(address underlying, uint256 maturity, address swivel, address yield, uint8 decimals) public onlyAdmin(admin) returns (bool) {
         
-        address illuminateAddress = address(new ZcToken(underlying, maturity, decimals, "illuminate", "ILL"));
+        address illuminateAddress = address(new ZcToken(underlying, maturity, "illuminate", "ILL", decimals));
         
-        Market memory market = Market(swivel,yield,illuminateAddress);
-        
-        markets[underlying][maturity] = market;
+        markets[underlying][maturity] = Market(swivel,yield,illuminateAddress);
         
         emit marketCreated(underlying, maturity, illuminateAddress, swivel, yield);
         
         return (true);
     }
-    
-    
+
+    /// @notice Can be called before maturity to wrap swivel zcTokens into illuminate tokens
+    /// @param underlying the underlying token being redeemed
+    /// @param maturity the maturity of the market being redeemed
+    /// @param amount the amount of underlying tokens to lend   
     function swivelMint(address underlying, uint256 maturity, uint256 amount) public returns (bool) {
         
         ZcToken illuminateToken = ZcToken(markets[underlying][maturity].illuminate);
@@ -82,26 +94,41 @@ contract Illuminate {
         
         return (true);
     }
-     
-    function swivelLend(address underlying, uint256 maturity, address swivelRouter, Hash.Order[] calldata orders, uint256[] calldata amounts, Sig.Components[] calldata signatures) public returns (bool) {
+
+    /// @notice Can be called before maturity to lend to Swivel while minting illuminate tokens
+    /// @param underlying the underlying token being redeemed
+    /// @param maturity the maturity of the market being redeemed
+    /// @param orders the swivel orders being filled
+    /// @param amounts the amount of underlying tokens lent to each order
+    function swivelLend(address underlying, uint256 maturity, Hash.Order[] calldata orders, uint256[] calldata amounts, Sig.Components[] calldata signatures) public returns (bool) {
 
         ZcToken illuminateToken = ZcToken(markets[underlying][maturity].illuminate);
         IERC20 underlyingToken = IERC20(underlying);
-        
+        uint256 totalLent;
+        uint256 totalReturned;
         for (uint256 i=0; i < orders.length; i++) {
-            underlyingToken.transferFrom(msg.sender, address(this), amounts[i]);
-            underlyingToken.approve(swivelRouter, amounts[i]);
+            Hash.Order memory order = orders[i];
+            uint256 amount = amounts[i];
+            totalLent += amount;
+            totalReturned += amount * order.premium / order.principal;
         }
         
-        require(SwivelRouter(swivelRouter).initiate(orders, amounts, signatures), "swivel minting failed"); 
+        underlyingToken.transferFrom(msg.sender, address(this), totalLent);
+        underlyingToken.approve(swivelRouter, totalLent);
+
+        require(SwivelRouter(swivelRouter).initiate(orders, amounts, signatures), "swivel lending failed"); 
         
-        for (uint256 i=0; i < orders.length; i++) {
-            illuminateToken.mint(msg.sender,amounts[i]);
-        }
+        uint256 yieldAmount = yieldLend(underlying, maturity, uint128(totalReturned));
+
+        illuminateToken.mint(msg.sender, (totalLent+yieldAmount));
         
         return (true);
     }
-    
+
+    /// @notice Can be called before maturity to wrap yield yTokens into illuminate tokens
+    /// @param underlying the underlying token being redeemed
+    /// @param maturity the maturity of the market being redeemed
+    /// @param amount the amount of underlying tokens to lend   
     function yieldMint(address underlying, uint256 maturity, uint256 amount) public returns (bool) {
         
         IFYDai yieldToken = IFYDai(markets[underlying][maturity].yield);
@@ -113,8 +140,12 @@ contract Illuminate {
         
         return (true);
     }
-    
-    function yieldLend(address underlying, uint256 maturity, address yieldRouter, uint128 amount) public returns (bool) {
+
+    /// @notice Can be called before maturity to lend to yield while minting illuminate tokens
+    /// @param underlying the underlying token being redeemed
+    /// @param maturity the maturity of the market being redeemed
+    /// @param amount the amount of underlying tokens to lend
+    function yieldLend(address underlying, uint256 maturity, uint128 amount) public returns (uint256) {
         
         IFYDai yieldToken = IFYDai(markets[underlying][maturity].yield);
         ZcToken illuminateToken = ZcToken(markets[underlying][maturity].illuminate);
@@ -125,9 +156,13 @@ contract Illuminate {
         
         illuminateToken.mint(msg.sender, fyDaiOut);
         
-        return (true);
+        return (fyDaiOut);
     }
     
+    /// @notice Can be called after maturity and after tokens have been redeemed to illuminate to redeem underlying tokens 
+    /// @param underlying the underlying token being redeemed
+    /// @param maturity the maturity of the market being redeemed
+    /// @param amount the amount of underlying tokens to redeem and illuminate tokens to burn
     function redeem(address underlying, uint256 maturity, uint256 amount) public returns (bool) {
     
         ZcToken illuminateToken = ZcToken(markets[underlying][maturity].illuminate);
@@ -140,29 +175,28 @@ contract Illuminate {
         return (true);
     }
     
-    function swivelRedeem(address underlying, uint256 maturity, address swivelMarketplace) public returns (bool) {
-    
-        IERC20 underlyingToken = IERC20(underlying);
+    /// @notice called at maturity to redeem all swivel zcTokenAddress to illuminate
+    /// @param underlying the underlying token being redeemed
+    /// @param maturity the maturity of the market being redeemed
+    function swivelRedeem(address underlying, uint256 maturity) public returns (bool) {
         
         uint256 amount = IERC20(markets[underlying][maturity].swivel).balanceOf(address(this));
         
-        require(SwivelMarketplace(swivelMarketplace).redeemZcToken(underlying,maturity,amount), "swivel redemption failed");
-        
-        underlyingToken.transfer(msg.sender,amount);
+        require(SwivelRouter(swivelRouter).redeemZcToken(underlying,maturity,amount), "swivel redemption failed");
         
         return (true);
     }
     
+    /// @notice called at maturity to redeem all yield yTokens to illuminate
+    /// @param underlying the underlying token being redeemed
+    /// @param maturity the maturity of the market being redeemed    
     function yieldRedeem(address underlying, uint256 maturity) public returns (bool) {
 
-        IERC20 underlyingToken = IERC20(underlying);
         IFYDai yieldToken = IFYDai(markets[underlying][maturity].illuminate);
 
         uint256 amount = yieldToken.balanceOf(address(this));
 
         require(yieldToken.redeem(address(this),address(this),amount) >= amount, "yield redemption failed");
-        
-        underlyingToken.transfer(msg.sender,amount);
         
         return (true);
     }
